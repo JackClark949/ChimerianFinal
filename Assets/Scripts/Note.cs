200e30337b9ae11e45b56e3d48ad099239fef3f5
DR676 <2306935@stu.chi.ac.uk>
2025-04-01 12:03:36 +0100

    UI stuff

--- Assets/Scripts/Note.cs
+++ Assets/Scripts/Note.cs
@@ -7,8 +7,10 @@ public class Note : MonoBehaviour
     public GameObject NoteUI;
     bool toggle;
     public playerMovement player;
+    public cameraController Camera;
     public Renderer NoteMesh;
 
+   
     public void OpenCloseNote()
     {
         toggle = !toggle;
@@ -17,12 +19,15 @@ public class Note : MonoBehaviour
             NoteUI.SetActive(false);
             NoteMesh.enabled = true;
             player.enabled = true;
+            Camera.enabled = true;
+
         }
-        if (toggle == true)
+        if(toggle == true)
         {
             NoteUI.SetActive(true);
             NoteMesh.enabled = false;
             player.enabled = false;
+            Camera.enabled = false;
         }
     }
 }
